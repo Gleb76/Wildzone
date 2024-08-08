e50cdaad11c3601d8404bb8245dc3ed3b67ec180
Gleb Klyga <klyga90@bk.ru>
2024-08-08 23:29:50 +0300

    initialize project

---
+++ Wildzone/Wildzone/WildzoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildzoneApp.swift
+//  Wildzone
+//
+//  Created by Глеб Клыга on 8.08.24.
+//
+
+import SwiftUI
+
+@main
+struct WildzoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
